91a81e09dfaa135cb202ddacc1c5b57319e9c2c0
Andrei Kamalov <andrei.kamalov@gmail.com>
2020-03-19 10:42:01 -0700

    code update

--- DataAcquisition130.py
+++ DataAcquisition130.py
@@ -6,14 +6,16 @@
 #import needed libraries
 import sys
 sys.path.append("C:/Users/andre/Documents/GitHub/DataAcquisition130/libraries")
-
+import numpy as np
 #import commands from libraries.
 #commands should be renamed as what they will be used as in the main executiong script
-#from simulatingWithCollectedData_lib import dataBufferRandomSimulation as getDataBuffered
 from simulatingWithCollectedData_lib import dataBufferRandomSimulation as getDataBuffered
+from simulatingWithCollectedData_lib import readInDataFromFolder as readInData
+from simulatingWithCollectedData_lib import performNoProcessing as onTheFlyProcessing
 #setup variables
 keepGoingFlag = True
-
+numChannels = 1 #currently, support for only one channel at a time is implemented
+processedDataToWrite = []
 #run program
 #initialize and setup lines
 
@@ -21,9 +23,22 @@ keepGoingFlag = True
 while keepGoingFlag:
 
 	if getDataBuffered():
-		print('reading in data')
+		#proceed to read out data into arrays
+		newData = readInData()
+
+		#perform on the fly processing for the arrays
+		processedData = onTheFlyProcessing(newData)
+
+		#send processed data to array that is wating to be written out from
+		processedDataToWrite.append(processedData)
+
+		breakpoint()
 	else:
-		print('writing out data')
+		pass
+		#write out any data in variable arrays that are awaiting to be written out
+
+		#update internal variables with written out data for plotting purposes
+		#check if data is now buffered
 
 
 print("end of loop reached")
--- libraries/simulatingWithCollectedData_lib.py
+++ libraries/simulatingWithCollectedData_lib.py
@@ -1,4 +1,23 @@
 import random
+import glob
+import lecroyparser
 
 def dataBufferRandomSimulation():
 	return random.getrandbits(1)
+
+
+dataFolderDirectory = 'C:\\Andrei\\dataTransfer\\03_16_2020_CX60_6013E-S+'
+globPathname = dataFolderDirectory+'\\*.trc'
+fileArray = glob.glob(globPathname)
+fileArrayIndex = 0
+
+def readInDataFromFolder():
+	global fileArrayIndex
+	fileNameNow = fileArray[fileArrayIndex]
+	dataNow = lecroyparser.ScopeData(fileNameNow)
+	fileArrayIndex += 1
+	return dataNow.y
+
+#method 'performNoProcessing' is designed as a stand in.  it does not actually do any processing, but rather returns the data provided immediately.
+def performNoProcessing(dataIn):
+	return dataIn
