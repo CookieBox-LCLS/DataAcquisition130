692e5641a4b3f5b93ec967a9efe87a7c8012b850
Andrei Kamalov <andrei.kamalov@gmail.com>
2020-03-24 18:03:23 -0700

    code update

--- DataAcquisition130.py
+++ DataAcquisition130.py
@@ -6,7 +6,7 @@
 #import needed libraries
 import sys
 sys.path.append("C:/Users/andre/Documents/GitHub/DataAcquisition130/libraries")
-import numpy as np
+import numpy
 import datetime
 #import commands from libraries.
 #commands should be renamed as what they will be used as in the main executiong script
@@ -14,6 +14,7 @@ from simulatingWithCollectedData_lib import dataBufferRandomSimulation as getDat
 from simulatingWithCollectedData_lib import readInDataFromFolder as readInData
 from simulatingWithCollectedData_lib import performNoProcessing as onTheFlyProcessing
 from simulatingWithCollectedData_lib import writeOutProcessedData as writeOut
+from simulatingWithCollectedData_lib import generateNewFileAndHeader as generateNewFileAndHeader
 #setup variables
 keepGoingFlag = True
 numChannels = 1 #currently, support for only one channel at a time is implemented
@@ -25,6 +26,20 @@ saveToDirectory = "C:\\Users\\andre\\Desktop\\DataWriteOut\\"#written out data t
 processedDataToWrite = []
 fileNameNow = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")#the file name that writeout data will be saved to
 fileNameNowFull = saveToDirectory+str(fileNameNow)
+
+#run a loop to generate file and place delimiter.  Do all one time events here, so that no further processing will be necessary
+entryLoopCompleted = False
+while not entryLoopCompleted:
+	if getDataBuffered:
+		#read in sacrificial first data into an array.
+		sampleData = readInData()
+		#apply processing.  this is done to see what the processed data looks like
+		processedData = onTheFlyProcessing(sampleData)
+		processedDataToWrite.append(processedData)
+		#take the sample data
+		processedDataToWrite = generateNewFileAndHeader(fileNameNowFull, processedDataToWrite)
+		entryLoopCompleted = True
+
 
 #begin main loop
 while keepGoingFlag:
@@ -39,7 +54,8 @@ while keepGoingFlag:
 		#send processed data to array that is wating to be written out from
 		processedDataToWrite.append(processedData)
 	else:
-		#write out any data in variable arrays that are awaiting to be written out
+		#write out any data in variable arrays that are awaiting to be written out.
+		#the array 'processedDataToWrite' is returned as an empty array that can be re-filled.  It serves as a temporary variable buffer between data acquisitions and data write-outs.
 		processedDataToWrite = writeOut(fileNameNowFull, processedDataToWrite)
 
 		#update internal variables with written out data for plotting purposes
--- libraries/simulatingWithCollectedData_lib.py
+++ libraries/simulatingWithCollectedData_lib.py
@@ -1,6 +1,8 @@
 import random
 import glob
 import lecroyparser
+import numpy
+import sys
 
 def dataBufferRandomSimulation():
 	return random.getrandbits(1)
@@ -26,12 +28,38 @@ def performNoProcessing(dataIn):
 #the method 'writeOutProcessedData' takes the temporary data buffer in the program, and writes it out to the file in fileNameNowFull
 def writeOutProcessedData(fileNameNowFull, processedDataToWrite):
 	#open the file to which post-on-the-fly-processed data is written.
-	file = open(str(fileNameNowFull), 'w')
+	file = open(str(fileNameNowFull), 'ab')
 	#the inbound list is a series associated with each readout trace
 	for i in range(len(processedDataToWrite)):
 		#for each element in the list, remove the first element and write it out
 		toWrite = processedDataToWrite.pop(0)
-		file.write("1")
+		toWrite.tofile(file, "")
 	file.close()
 
+	return processedDataToWrite
+
+
+#method 'generateNewFileAndHeader' is designed to look at a sampling of data and get it's binary data size, and output it to the file's associated header.txt file.  It then creates the actual data file and saves the first bit of sampled data to it, so as to not waste it.
+def generateNewFileAndHeader(fileNameNow, processedDataToWrite):
+	####figure out the header file first
+	fileNameHeader = fileNameNow + '_HEADER.txt'
+	#create and open the file that will be used to store processed data.
+	file = open(str(fileNameHeader), 'w')
+	#there should only be one entry here - use it to gauge length of write out, and create a header with that information.
+	toWrite = processedDataToWrite.pop(0)
+	#get binary size of data sample
+	binaryDataSize = sys.getsizeof(toWrite)
+	#output size of binary data
+	file.write("The size of an individual bit of binary data is: " + str(binaryDataSize) + "\n")
+	#close file
+	file.close()
+
+	#####also save the sampled binary data to the real file so as to not waste it.
+	#open the file to which post-on-the-fly-processed data is written.
+	file = open(str(fileNameNow), 'ab')
+	#the portion of binary data that needs saving is the single trace pop'd off of the array earlier.
+	toWrite.tofile(file, "")
+	file.close()
+
+	#the empty array is returned so that the loop that called this function knows that there is nothing else to read out of the array.
 	return processedDataToWrite
